86b2484f35f1afd2c0b0cc0c276f85c4c56eef82
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2024-07-31 22:55:09 +0700

    setup color util

---
+++ lib/util/colors/color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color cWhite = Color(0xFFF3F2ED);
+
+const Color cPrimary100 = Color(0xFFB22222);
+const Color cPrimary200 = Color(0xFFBE4237);
+const Color cPrimary300 = Color(0xFFCA5C4E);
+const Color cPrimary400 = Color(0xFFD57465);
+const Color cPrimary500 = Color(0xFFDE8B7D);
+const Color cPrimary600 = Color(0xFFE7A295);
+
+const Color cDark100 = Color(0xFF202124);
+const Color cDark200 = Color(0xFF353638);
+const Color cDark300 = Color(0xFF4B4C4E);
+const Color cDark400 = Color(0xFF626365);
+const Color cDark500 = Color(0xFF7A7B7D);
+const Color cDark600 = Color(0xFF939495);
