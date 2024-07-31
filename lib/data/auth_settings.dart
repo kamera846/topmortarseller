3bcf95fd70fb04211ef5d5fc3283ac9c875cf7ce
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2024-08-01 04:43:38 +0700

    Setup Auth Page Settings

---
+++ lib/data/auth_settings.dart
@@ -0,0 +1,25 @@
+import 'package:topmortarseller/models/auth_settings_model.dart';
+
+const authSettings = {
+  AuthType.login: AuthSettingsModel(
+    'Top Mortar Seller',
+    'Gunakan akun yang terdaftar untuk melanjutkan.',
+    'Masuk',
+    'Belum punya akun?',
+    'Daftar Sekarang!',
+  ),
+  AuthType.register: AuthSettingsModel(
+    'Daftar Akun',
+    'Cek terlebih dahulu apakah nomor anda sudah terdaftar.',
+    'Cek Nomor',
+    'Sudah punya akun?',
+    'Kembali',
+  ),
+  AuthType.forgot: AuthSettingsModel(
+    'Reset Password',
+    'Masukan nomor untuk mendapatkan kode otp reset password.',
+    'Dapatkan OTP',
+    'Sudah ingat sandi anda?',
+    'Kembali',
+  )
+};
---
+++ lib/models/auth_settings_model.dart
@@ -0,0 +1,21 @@
+enum AuthType {
+  login,
+  register,
+  forgot,
+}
+
+class AuthSettingsModel {
+  const AuthSettingsModel(
+    this.title,
+    this.description,
+    this.elevatedButtonText,
+    this.outlinedButtonDescription,
+    this.outlinedButtonText,
+  );
+
+  final String title;
+  final String description;
+  final String elevatedButtonText;
+  final String outlinedButtonDescription;
+  final String outlinedButtonText;
+}
