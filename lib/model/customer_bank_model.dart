27f13ea4714f0a3d33a1757ad28939c0b4fefaf9
M Rafli Ramadani <keerouk.ink@gmail.com>
2024-08-13 16:45:29 +0700

    setup customer bank model api

---
+++ lib/model/customer_bank_model.dart
@@ -0,0 +1,53 @@
+class CustomerBankModel {
+  const CustomerBankModel({
+    this.idRekeningRoko,
+    this.idContact,
+    this.toName,
+    this.idBank,
+    this.toAccount,
+    this.createdAt,
+    this.updatedAt,
+    this.namaBank,
+    this.swiftBank,
+    this.isBca,
+  });
+
+  final String? idRekeningRoko;
+  final String? idContact;
+  final String? toName;
+  final String? idBank;
+  final String? toAccount;
+  final String? createdAt;
+  final String? updatedAt;
+  final String? namaBank;
+  final String? swiftBank;
+  final String? isBca;
+
+  Map<String, dynamic> toJson() => {
+        'id_rekening_toko': idRekeningRoko ?? '',
+        'id_contact': idContact ?? '',
+        'to_name': toName ?? '',
+        'id_bank': idBank ?? '',
+        'to_account': toAccount ?? '',
+        'created_at': createdAt ?? '',
+        'updated_at': updatedAt ?? '',
+        'nama_bank': namaBank ?? '',
+        'swift_bank': swiftBank ?? '',
+        'is_bca': isBca ?? '',
+      };
+
+  factory CustomerBankModel.fromJson(Map<String, dynamic> json) {
+    return CustomerBankModel(
+      idRekeningRoko: json['id_rekening_toko'] ?? '',
+      idContact: json['id_contact'] ?? '',
+      toName: json['to_name'] ?? '',
+      idBank: json['id_bank'] ?? '',
+      toAccount: json['to_account'] ?? '',
+      createdAt: json['created_at'] ?? '',
+      updatedAt: json['updated_at'] ?? '',
+      namaBank: json['nama_bank'] ?? '',
+      swiftBank: json['swift_bank'] ?? '',
+      isBca: json['is_bca'] ?? '',
+    );
+  }
+}
---
+++ lib/services/customer_bank.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:topmortarseller/model/customer_bank_model.dart';
+import 'package:topmortarseller/services/api.dart';
+
+class CustomerBankApiService {
+  Future<CustomerBankModel?> banks({
+    required String idContact,
+    required Function(String e) onSuccess,
+    required Function(String e) onError,
+    required Function() onCompleted,
+  }) async {
+    CustomerBankModel? item;
+    try {
+      final url = Uri.https(baseUrl, 'api/rekeningtoko/contact/$idContact');
+      final response = await http.get(
+        url,
+        headers: headerSetup,
+      );
+
+      if (response.statusCode == 200) {
+        final responseBody = json.decode(response.body);
+        final apiResponse = ApiResponse.fromJson(responseBody);
+
+        if (apiResponse.code == 200) {
+          if (apiResponse.data != null) {
+            item = CustomerBankModel.fromJson(apiResponse.data!);
+          }
+          onSuccess(apiResponse.msg);
+          return item;
+        }
+
+        onError(apiResponse.msg);
+        return item;
+      } else {
+        onError('$failedRequestText. Status Code: ${response.statusCode}');
+        return item;
+      }
+    } catch (e) {
+      onError('$failedRequestText. Exception: $e');
+      return item;
+    } finally {
+      onCompleted();
+    }
+  }
+
+  Future<CustomerBankModel?> newBank({
+    required String idContact,
+    required String idBank,
+    required String nameRek,
+    required String noRek,
+    required Function(String e) onSuccess,
+    required Function(String e) onError,
+    required Function() onCompleted,
+  }) async {
+    CustomerBankModel? item;
+    try {
+      final url = Uri.https(baseUrl, 'api/rekeningtoko');
+      final response = await http.post(url, headers: headerSetup, body: {
+        'id_contact': idContact,
+        'id_bank': idBank,
+        'to_name': nameRek,
+        'to_account': noRek,
+      });
+
+      if (response.statusCode == 200) {
+        final responseBody = json.decode(response.body);
+        final apiResponse = ApiResponse.fromJson(responseBody);
+
+        if (apiResponse.code == 200) {
+          if (apiResponse.data != null) {
+            item = CustomerBankModel.fromJson(apiResponse.data!);
+          }
+          onSuccess(apiResponse.msg);
+          return item;
+        }
+
+        onError(apiResponse.msg);
+        return item;
+      } else {
+        onError('$failedRequestText. Status Code: ${response.statusCode}');
+        return item;
+      }
+    } catch (e) {
+      onError('$failedRequestText. Exception: $e');
+      return item;
+    } finally {
+      onCompleted();
+    }
+  }
+}
