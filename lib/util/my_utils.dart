bfd8752ca4f7be40490509b5742749a8e16651f3
Mochammad Rafli Ramadani <mochammadrafliramadani@gmail.com>
2025-10-30 00:11:57 +0700

    navigate to webview if scanner result is url

---
+++ lib/util/my_utils.dart
@@ -0,0 +1,12 @@
+class MyUtils {
+  static bool isValidUrl(String url) {
+    try {
+      final uri = Uri.parse(url);
+      return uri.isAbsolute &&
+          (uri.scheme == 'http' || uri.scheme == 'https') &&
+          uri.hasAuthority;
+    } catch (e) {
+      return false;
+    }
+  }
+}
